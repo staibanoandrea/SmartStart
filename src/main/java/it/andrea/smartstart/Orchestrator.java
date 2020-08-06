85c4a9332e7854536738b333178084d39105a3e9
nello <agnello1984@gmail.com>
2020-08-06 16:16:43 +0200

    vedi un po'

--- src/main/java/it/andrea/smartstart/Orchestrator.java
+++ src/main/java/it/andrea/smartstart/Orchestrator.java
@@ -3,13 +3,36 @@ package it.andrea.smartstart;
 import java.util.List;
 
 import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPubSub;
 
-public class Orchestrator {
-	private void subscribe(String channel) {
-		Subscriber subscriber = new Subscriber(channel);
-	}
-	
-	private List<Request> translate() {
-		return null;
-	}
+public class Orchestrator extends JedisPubSub {
+
+    private Jedis jedis;
+    private Crane crane;
+    private Bin bin;
+
+    public Orchestrator() {
+        super();
+        jedis = new Jedis();
+        subscribe();
+    }
+
+    private void subscribe() {
+        jedis.subscribe(this, "channel");
+    }
+
+    @Override
+    public void onMessage(String channel, String message) {
+        System.out.println("Channel " + channel + " has sent a message : " + message);
+        switch (channel) {
+        case "a":
+            break;
+        default:
+            break;
+        }
+    }
+
+    private List<Request> translate() {
+        return null;
+    }
 }
