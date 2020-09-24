8769c2969aa531647acecbd03658ffa159dbc5a1
Andrea <andreastaibano95@gmail.com>
2020-09-24 18:23:16 +0200

    Ottimizzazione di createSubset e della gestione del tempo

--- src/main/java/it/andrea/smartstart/Evaluator.java
+++ src/main/java/it/andrea/smartstart/Evaluator.java
@@ -5,6 +5,7 @@ import java.math.RoundingMode;
 import java.util.List;
 
 public class Evaluator {
+	private Integer craneSize;
 	private Double time = 0.0;
 	private int satisfiedRequests = 0; // requests satisfied so far;
 	private int[] satisfiedRequestsBySize; // contains the count of the subset served so far, divided by size;
@@ -13,45 +14,67 @@ public class Evaluator {
 	private Double maxTravelTime = 0.0; // max of all the subsets so far;
 	private Double totWaitingTime = 0.0; // total time waited from every single request;
 	private Double maxWaitingTime = 0.0; // max time waited from every single request;
+	private Integer maxWaitingIndex;
 
 	public Evaluator(int craneSize) {
+		this.craneSize = craneSize;
 		this.satisfiedRequestsBySize = new int[craneSize];
 	}
 
 	public void run(List<Subset> craneWorkSheet) {
 		for (Subset subset : craneWorkSheet) {
+
+			// get data about the whole subset:
 			Double travelDistance = TourCalculator.getTotalDistance(subset.getRequestList());
 			if (travelDistance > maxTravelTime) {
 				maxTravelTime = travelDistance;
 			}
 			idleTime += subset.getTimeWaited();
-			Double startTime = time + subset.getTimeWaited();
 			totalDistance += travelDistance;
 			time += subset.getTimeWaited() + travelDistance;
+			satisfiedRequestsBySize[subset.getRequestList().size() - 1]++;
+
+			// get data about the single requests:
 			for (Request request : subset.getRequestList()) {
 				satisfiedRequests++;
 				Double timeWaited = time - request.getRequestTime();
 				totWaitingTime += timeWaited;
 				if (timeWaited > maxWaitingTime) {
 					maxWaitingTime = timeWaited;
+					maxWaitingIndex = request.getIndex();
 				}
 			}
-			System.out.println("Subset " + subset.getIndex() + ": " + subset.print() + " started at "
-					+ new BigDecimal(startTime).setScale(5, RoundingMode.HALF_UP) + " and traveled "
-					+ new BigDecimal(travelDistance).setScale(5, RoundingMode.HALF_UP) + " space units;");
+
+			// print subset-by-subset data:
+			System.out.println("Subset " + subset.getIndex() + ": " + subset.print() + " started after "
+					+ new BigDecimal(subset.getTimeWaited()).setScale(5, RoundingMode.HALF_UP) + " and traveled "
+					+ new BigDecimal(travelDistance).setScale(5, RoundingMode.HALF_UP) + " space units, coming back at "
+					+ new BigDecimal(time).setScale(5, RoundingMode.HALF_UP) + ";");
 		}
+		// print recap:
 		System.out.println();
 		System.out.println("Number of travels: " + craneWorkSheet.size());
 		System.out.println("Time spent idle: " + new BigDecimal(idleTime).setScale(5, RoundingMode.HALF_UP));
 		System.out.println("Time spent travelling: " + new BigDecimal(totalDistance).setScale(5, RoundingMode.HALF_UP));
 		System.out.println("Average travel time: "
 				+ new BigDecimal(totalDistance / craneWorkSheet.size()).setScale(5, RoundingMode.HALF_UP));
-		System.out.println("Maximum travel time: "
-				+ new BigDecimal(maxTravelTime).setScale(5, RoundingMode.HALF_UP));
+		System.out.println("Maximum travel time: " + new BigDecimal(maxTravelTime).setScale(5, RoundingMode.HALF_UP));
 		System.out.println();
 		System.out.println("Average time waited by all requests: "
 				+ new BigDecimal(totWaitingTime / satisfiedRequests).setScale(5, RoundingMode.HALF_UP));
 		System.out.println("Maximum time waited by a request: "
-				+ new BigDecimal(maxWaitingTime).setScale(5, RoundingMode.HALF_UP));
+				+ new BigDecimal(maxWaitingTime).setScale(5, RoundingMode.HALF_UP) + " by request " + maxWaitingIndex);
+		System.out.println();
+
+		Double craneUsage = (double) satisfiedRequests / craneWorkSheet.size() / craneSize * 100;
+		System.out.println("Crane usage: " + new BigDecimal(craneUsage).setScale(2, RoundingMode.HALF_UP) + "%");
+		System.out.println("Crane usage distribution: ");
+		for (int i = 1; i <= craneSize; i++) {
+			System.out.print("[" + i + "]\t");
+		}
+		System.out.println();
+		for (int i = 0; i < craneSize; i++) {
+			System.out.print("[" + satisfiedRequestsBySize[i] + "]\t");
+		}
 	}
 }
--- src/main/java/it/andrea/smartstart/SmartStart.java
+++ src/main/java/it/andrea/smartstart/SmartStart.java
@@ -7,11 +7,11 @@ import java.util.List;
 public class SmartStart {
 	public static Request origin;
 	private static final int originIndex = 0;
-	
+
 	public static void main(String[] args) {
 		/*
-		 * expected line:
-		 * java SmartStart alpha(double) craneSize(int) timeFile(file) coordFile(file) [mode(int)]
+		 * expected line: java SmartStart alpha(double) craneSize(int) timeFile(file)
+		 * coordFile(file) [mode(int)]
 		 */
 		// there are no input checks
 		final double alpha = Double.parseDouble(args[0]);
@@ -23,31 +23,37 @@ public class SmartStart {
 		if (args.length == 5) {
 			mode = Integer.parseInt(args[4]);
 		}
-		
+
 		List<Subset> craneWorkSheet = new ArrayList<Subset>();
-		
+
 		// create the list of requests from files:
 		List<Request> requestList = Reader.translateFile(timeFile, coordFile);
-		
+
 		// pop the origin:
 		origin = requestList.get(originIndex);
 		requestList.remove(originIndex);
-		
+
 		// MODE 0: always serve the best subset first, with no other priorities:
 		if (mode == 0) {
+			int index = 0;
+			double craneStartTime = Double.MAX_VALUE; // the last time the crane left the origin;
+			double craneBackTime = 0.0; // the last time the crane went back to the origin;
+
 			// iteratively get the best subset to serve, and remove it from the list:
-			int i = 0;
-			double time = 0.0;
 			while (!requestList.isEmpty()) {
-				i++;
-				Subset subset = createSubset(i, alpha, craneSize, requestList, time);
-				time = execute(subset, time); // placeholder call for crane to work, actually moves time
+				index++;
+				Subset subset = createSubset(index, alpha, craneSize, requestList, craneStartTime, craneBackTime);
+				// craneStartTime is updated to when the subset is confirmed and served:
+				craneStartTime = craneBackTime + subset.getTimeWaited();
+				// craneBackTime is updated accordingly:
+				craneBackTime = craneStartTime + TourCalculator.getTotalDistance(subset.getRequestList());
+				execute(subset); // placeholder call for crane to work
 				craneWorkSheet.add(subset);
 				requestList.removeAll(subset.getRequestList());
 			}
 		}
-		
-		// evaluate the created subset:
+
+		// evaluate the created craneWorkSheet:
 		Evaluator evaluator = new Evaluator(craneSize);
 		evaluator.run(craneWorkSheet);
 	}
@@ -55,23 +61,36 @@ public class SmartStart {
 	/*
 	 * createSubset takes the whole requestList, but uses it in an online fashion:
 	 */
-	public static Subset createSubset(int index, double alpha, int craneSize, List<Request> requestList, double time) {
+	public static Subset createSubset(int index, double alpha, int craneSize, List<Request> requestList,
+			double craneStartTime, double craneBackTime) {
 		List<Request> subsetList = new ArrayList<Request>();
 		List<Request> bestRSubset = new ArrayList<Request>();
 		Double timeLimit = Double.MAX_VALUE; // waits forever until it gets the first request;
 		for (Request r : requestList) {
-			if (r.getRequestTime() > timeLimit + time) { // if the request arrives after the timelimit, it is excluded from the subset:
-				break;
-			} else { // add the request to the subset and compute the new timelimit for the next request:
+			// CASE 1: the request arrives while the crane is idling:
+			if (r.getRequestTime() >= craneBackTime) {
+				if (r.getRequestTime() > timeLimit + craneBackTime) {
+					break;
+				} else {
+					subsetList.add(r);
+					bestRSubset = TourCalculator.kPermutate(craneSize, subsetList);
+					timeLimit = alpha * TourCalculator.getTotalDistance(bestRSubset);
+				}
+			}
+			// CASE 2: the request arrives while the crane is still traveling:
+			else {
 				subsetList.add(r);
-				bestRSubset = TourCalculator.kPermutate(craneSize, subsetList); // this is a temporary best subset; it becomes definitive if the next request is too late;
-				timeLimit = alpha * TourCalculator.getTotalDistance(bestRSubset);
+				bestRSubset = TourCalculator.kPermutate(craneSize, subsetList);
+				if (craneStartTime + alpha * TourCalculator.getTotalDistance(bestRSubset) > craneBackTime) {
+					timeLimit = craneStartTime + alpha * TourCalculator.getTotalDistance(bestRSubset) - craneBackTime;
+				} else {
+					timeLimit = 0.0;
+				}
 			}
 		}
 		return new Subset(index, bestRSubset, timeLimit);
 	}
 
-	private static double execute(Subset subset, double time) {
-		return time + TourCalculator.getTotalDistance(subset.getRequestList());
+	private static void execute(Subset subset) {
 	}
 }
