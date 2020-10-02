c0c00be79fe3d98b4e92bc693ac31c319ba23e39
Andrea <andreastaibano95@gmail.com>
2020-10-02 10:57:29 +0200

    Aggiunte modalità 1 e 2

--- src/main/java/it/andrea/smartstart/SmartStart.java
+++ src/main/java/it/andrea/smartstart/SmartStart.java
@@ -33,24 +33,21 @@ public class SmartStart {
 		origin = requestList.get(originIndex);
 		requestList.remove(originIndex);
 
-		// MODE 0: always serve the best subset first, with no other priorities:
-		if (mode == 0) {
-			int index = 0;
-			double craneStartTime = Double.MAX_VALUE; // the last time the crane left the origin;
-			double craneBackTime = 0.0; // the last time the crane went back to the origin;
+		int index = 0;
+		double craneStartTime = Double.MAX_VALUE; // the last time the crane left the origin;
+		double craneBackTime = 0.0; // the last time the crane went back to the origin;
 
-			// iteratively get the best subset to serve, and remove it from the list:
-			while (!requestList.isEmpty()) {
-				index++;
-				Subset subset = createSubset(index, alpha, craneSize, requestList, craneStartTime, craneBackTime);
-				// craneStartTime is updated to when the subset is confirmed and served:
-				craneStartTime = craneBackTime + subset.getTimeWaited();
-				// craneBackTime is updated accordingly:
-				craneBackTime = craneStartTime + TourCalculator.getTotalDistance(subset.getRequestList());
-				execute(subset); // placeholder call for crane to work
-				craneWorkSheet.add(subset);
-				requestList.removeAll(subset.getRequestList());
-			}
+		// iteratively get the best subset to serve, and remove it from the list:
+		while (!requestList.isEmpty()) {
+			index++;
+			Subset subset = createSubset(index, alpha, craneSize, requestList, craneStartTime, craneBackTime, mode);
+			// craneStartTime is updated to when the subset is confirmed and served:
+			craneStartTime = craneBackTime + subset.getTimeWaited();
+			// craneBackTime is updated accordingly:
+			craneBackTime = craneStartTime + TourCalculator.getTotalDistance(subset.getRequestList());
+			execute(subset); // placeholder call for crane to work
+			craneWorkSheet.add(subset);
+			requestList.removeAll(subset.getRequestList());
 		}
 
 		// evaluate the created craneWorkSheet:
@@ -62,32 +59,79 @@ public class SmartStart {
 	 * createSubset takes the whole requestList, but uses it in an online fashion:
 	 */
 	public static Subset createSubset(int index, double alpha, int craneSize, List<Request> requestList,
-			double craneStartTime, double craneBackTime) {
+			double craneStartTime, double craneBackTime, int mode) {
 		List<Request> subsetList = new ArrayList<Request>();
 		List<Request> bestRSubset = new ArrayList<Request>();
 		Double timeLimit = Double.MAX_VALUE; // waits forever until it gets the first request;
-		for (Request r : requestList) {
-			// CASE 1: the request arrives while the crane is idling:
-			if (r.getRequestTime() >= craneBackTime) {
-				if (r.getRequestTime() > timeLimit + craneBackTime) {
-					break;
-				} else {
+
+		// MODE 0: always serve the best subset first, with no other priorities:
+		if (mode == 0) {
+			for (Request r : requestList) {
+				// CASE 1: the request arrives while the crane is idling:
+				if (r.getRequestTime() >= craneBackTime) {
+					if (r.getRequestTime() > timeLimit + craneBackTime) {
+						break;
+					} else {
+						subsetList.add(r);
+						bestRSubset = TourCalculator.kPermutate(craneSize, subsetList);
+						timeLimit = alpha * TourCalculator.getTotalDistance(bestRSubset);
+					}
+				}
+				// CASE 2: the request arrives while the crane is still traveling:
+				else {
 					subsetList.add(r);
 					bestRSubset = TourCalculator.kPermutate(craneSize, subsetList);
-					timeLimit = alpha * TourCalculator.getTotalDistance(bestRSubset);
-				}
-			}
-			// CASE 2: the request arrives while the crane is still traveling:
-			else {
-				subsetList.add(r);
-				bestRSubset = TourCalculator.kPermutate(craneSize, subsetList);
-				if (craneStartTime + alpha * TourCalculator.getTotalDistance(bestRSubset) > craneBackTime) {
-					timeLimit = craneStartTime + alpha * TourCalculator.getTotalDistance(bestRSubset) - craneBackTime;
-				} else {
-					timeLimit = 0.0;
+					if (craneStartTime + alpha * TourCalculator.getTotalDistance(bestRSubset) > craneBackTime) {
+						timeLimit = craneStartTime + alpha * TourCalculator.getTotalDistance(bestRSubset)
+								- craneBackTime;
+					} else {
+						timeLimit = 0.0;
+					}
 				}
 			}
 		}
+
+		// MODE 1: first come, first served. Always fill the crane:
+		if (mode == 1) {
+			for (int i = 0; i < craneSize; i++) {
+				bestRSubset.add(requestList.get(i));
+			}
+			timeLimit = Math.max(bestRSubset.get(craneSize - 1).getRequestTime() - craneBackTime, 0);
+		}
+
+		// MODE 2: first come, first served. Use timeLimit to decide whether to wait:
+		if (mode == 2) {
+			for (Request r : requestList) {
+				// CASE 1: the request arrives while the crane is idling:
+				if (r.getRequestTime() >= craneBackTime) {
+					if (r.getRequestTime() > timeLimit + craneBackTime) {
+						break;
+					} else {
+						bestRSubset.add(r);
+						timeLimit = alpha * TourCalculator.getTotalDistance(bestRSubset);
+					}
+				}
+				// CASE 2: the request arrives while the crane is still traveling:
+				else {
+					bestRSubset.add(r);
+					if (craneStartTime + alpha * TourCalculator.getTotalDistance(bestRSubset) > craneBackTime) {
+						timeLimit = craneStartTime + alpha * TourCalculator.getTotalDistance(bestRSubset)
+								- craneBackTime;
+					} else {
+						timeLimit = 0.0;
+					}
+				}
+				// if the crane is full, start the crane:
+				if (bestRSubset.size() == craneSize) {
+					timeLimit = Math.max(bestRSubset.get(craneSize - 1).getRequestTime() - craneBackTime, 0);
+					break;
+				}
+			}
+		}
+		
+		// TODO
+		// MODE 3: same as mode 0, but prevent requests starvation:
+
 		return new Subset(index, bestRSubset, timeLimit);
 	}
 
